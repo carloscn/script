675a1f1ac90f76b1113dc376469a95b738b17c9a
Carlos <carlos.wei.hk@gmail.com>
2023-02-18 18:52:56 +0800

    [cunit] add cunit compile
    Signed-off-by: Carlos <carlos.wei.hk@gmail.com>

---
+++ build_cunit/build_cunit_arm.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+CROSS_COMPILE=arm-linux-gnueabihf
+HOST=arm-linux-gnueabihf
+
+function __utils_check_ret() {
+
+    if [ $1 -eq 0 ]; then
+        echo "[INFO] $2 done!"
+    else
+        echo "[ERR] Failed on $2!"
+        exit -1
+    fi
+}
+
+target_ball=CUnit-2.1-3.tar.bz2
+target_path=CUnit-2.1-3
+
+if [ ! -f ${target_ball} ];then
+    aria2c -x5 "https://github.com/carloscn/CUnit/releases/download/CUnit-2.1-3/CUnit-2.1-3.tar.bz2"
+    __utils_check_ret $? "download CUnit failed!"
+fi
+
+tar -xvf ${target_ball}
+__utils_check_ret $? "tar -xvf failed!"
+
+cd ${target_path}
+
+aclocal
+autoconf
+automake
+automake --add-missing
+
+CC=${CROSS_COMPILE} ./configure --prefix=`pwd`/out --host=${HOST}
+
+make -j8
+
+make install
+
+tree -L2 ./out
---
+++ build_cunit/build_cunit_mac.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+function __utils_check_ret() {
+
+    if [ $1 -eq 0 ]; then
+        echo "[INFO] $2 done!"
+    else
+        echo "[ERR] Failed on $2!"
+        exit -1
+    fi
+}
+
+target_ball=CUnit-2.1-3.tar.bz2
+target_path=CUnit-2.1-3
+
+if [ ! -f ${target_ball} ];then
+    aria2c -x5 "https://github.com/carloscn/CUnit/releases/download/CUnit-2.1-3/CUnit-2.1-3.tar.bz2"
+    __utils_check_ret $? "download CUnit failed!"
+fi
+
+tar -xvf ${target_ball}
+__utils_check_ret $? "tar -xvf failed!"
+
+cd ${target_path}
+
+aclocal
+autoconf
+autoreconf -i
+automake
+
+./configure --prefix=`pwd`/out
+
+make -j8
+
+make install
+
+sudo cp -r ./out/include/* /usr/local/include/
+sudo cp -r ./out/lib/{*.dylib,*.a,*.la} /usr/local/lib/
